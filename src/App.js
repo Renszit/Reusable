0ad42eaed5f6b618542474715451df4080261d58
rensp <renspennings@gmail.com>
2021-02-08 22:52:13 +0100

    finished reusable component progress bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import ProgressBar from "./progressbar";
-import { useState } from "react";
 
 function App() {
-    
-
     return (
         <div className="App">
             <header className="App-header">
-                <ProgressBar percentage={20} />
-                {/* <ProgressBar value={40} minValue={0} maxValue={200} /> */}
+                <ProgressBar percentage={70} />
+                <ProgressBar value={40} minValue={0} maxValue={100} />
             </header>
         </div>
     );
--- src/progressbar.js
+++ src/progressbar.js
@@ -1,19 +1,62 @@
 import "./progressbar.css";
 import { useState, useEffect } from "react";
 
-const ProgressBar = ({ width, percentage }) => {
-    const [value, setValue] = useState(0);
+const ProgressBar = ({ percentage, value, minValue, maxValue }) => {
+    const [width, setWidth] = useState(0);
+    const [tooLarge, setTooLarge] = useState();
+    const [tooSmall, setTooSmall] = useState();
 
     useEffect(() => {
-        setValue(percentage * width);
-    }, [percentage, width]);
+        if (percentage) {
+            if (percentage > 100) {
+                console.log(
+                    "percentage is too high, can not be more than 100. Your percentage is:",
+                    percentage
+                );
+                setTooLarge(true);
+            } else if (percentage < 0) {
+                console.log(
+                    "percentage is too low, can not be less than 0. Your percentage is:",
+                    percentage
+                );
+                setTooSmall(true);
+            } else {
+                setTooLarge(false);
+                setTooSmall(false);
+                setWidth(percentage);
+            }
+        } else {
+            let percent = ((value - minValue) * 100) / (maxValue - minValue);
+            if (percent > 100) {
+                console.log(
+                    "percentage is too high, can not be more than 100. Your percentage is:",
+                    percentage
+                );
+                setTooLarge(true);
+            } else if (percent < 0) {
+                console.log(
+                    "percentage is too low, can not be less than 0. Your percentage is:",
+                    percentage
+                );
+                setTooSmall(true);
+            } else {
+                setTooLarge(false);
+                setTooSmall(false);
+                setWidth(percent);
+            }
+        }
+    }, [percentage, value, minValue, maxValue]);
 
     return (
         <div>
             <div className="progressbar">
-                <div style={{ width: `${value}px` }} className="progress">
-                    {value}%
-                </div>
+                {tooSmall && <div className="tooSmall">🙅‍♂️</div>}
+                {!tooSmall && !tooLarge && (
+                    <div style={{ width: `${width}%` }} className="progress">
+                        {width}%
+                    </div>
+                )}
+                {tooLarge && <div className="tooLarge">🙅‍♂️</div>}
             </div>
         </div>
     );
